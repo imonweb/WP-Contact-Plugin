a1427dfcbb7703876d910c5bd0723ac3fa7ec317
Imon <contact@imonweb.co.uk>
2023-01-19 22:56:46 +0000

    initial commit

---
+++ contact-plugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Contact-Plugin
+ * Plugin URI: https://github.com/imonweb/contact-plugin
+ * Description: Contact-Plugin
+ * Author: Imon
+ * Author URI: https://www.imonweb.co.uk
+ * Text-Domain: contact-plugin
+ * Version: 0.1.0
+ * License: GPL2
+ * License URL: https://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: contact-plugin
+ **/
+
+if( !defined('ABSPATH') )
+{
+  die('You cannot be here!');
+}
+
+if(!class_exists('ContactPlugin')){
+
+ 
+  class ContactPlugin {
+
+  }
+
+  new ContactPlugin;
+  
+}
