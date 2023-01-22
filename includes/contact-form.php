319706e4d779c9eb665c0dca4197e2e7c3baa1da
Imon <contact@imonweb.co.uk>
2023-01-22 23:35:18 +0000

    permission callback error

--- includes/contact-form.php
+++ includes/contact-form.php
@@ -2,8 +2,25 @@
 
 add_shortcode( 'contact', 'show_contact_form' );
 
+add_action( 'rest_api_init', 'create_rest_endpoint' );
+
 function show_contact_form() 
 {
   // return 'our form should load';
   include MY_PLUGIN_PATH . '/includes/templates/contact-form.php';
+}
+
+function create_rest_endpoint()
+{
+  register_rest_route( 'v1/contact-form', 'submit', array(
+    'methods'   =>  'POST',
+    'callback'  =>  'handle_enquiry',
+    // 'permission_callback' => '__return_true'
+    'permission_callback' => function() { return true; }
+  ) );
+}
+
+function handle_enquiry()
+{
+   echo 'hello';
 }
--- includes/templates/contact-form.php
+++ includes/templates/contact-form.php
@@ -1,30 +1,41 @@
 
-<div class="row mt-3">
+<div class="row mt-5">
   <div class="col-md-6 offset-md-3">
-   
-   
 
-    <div class="mb-3">
-      <input type="text" class="form-control" placeholder="Name" name="name">
-    </div>
-    <div class="mb-3">
-      <input type="text" class="form-control" placeholder="Email" name="email">
-    </div>
-    <div class="mb-3">
-      <input type="text" class="form-control" placeholder="Phone" name="phone">
-    </div>
-    <div class="mb-3">
-      <textarea name="message" class="form-control"></textarea>
-    </div>
-    <div class="mb-3">
-      <button type="submit" class="btn btn-primary">Submit Form</button>
-    </div>
+    <form id="enquiry_form">
+      <div class="mb-3">
+        <input type="text" class="form-control" placeholder="Name" name="name">
+      </div>
+      <div class="mb-3">
+        <input type="text" class="form-control" placeholder="Email" name="email">
+      </div>
+      <div class="mb-3">
+        <input type="text" class="form-control" placeholder="Phone" name="phone">
+      </div>
+      <div class="mb-3">
+        <textarea name="message" class="form-control"></textarea>
+      </div>
+      <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Submit Form</button>
+      </div>
+    </form>
     
   </div>
 </div>
 
- 
-
-
-
-<h1 class="bg-red-500">Hello</h1>
+<script>
+  jQuery(document).ready(function($){
+    $("#enquiry_form").submit( function(event){
+      event.preventDefault();
+      // alert('ok');
+      const form = $(this);
+      console.log(form.serialize());
+      $.ajax({
+        type: "POST",
+        url: "<?php echo get_rest_uri(null, 'v1/contact-form/submit' ); ?>",
+        data: form.serialize()
+      });
+    });
+  });
+</script>
+  
